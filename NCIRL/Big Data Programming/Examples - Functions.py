88fce678e76ae3365f50bce130e357a28da20cfb
Paul Finn <paul.finn@outlook.com>
2016-10-07 21:46:52 +0100

    week 3 work

---
+++ NCIRL/Big Data Programming/Examples - Functions.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      finnp
+#
+# Created:     07/10/2016
+# Copyright:   (c) finnp 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+import antigravity
+
+def salutation(name):
+    salutation = "hello " + name
+    print(salutation)
+
+
+salutation(raw_input('Enter your Name: '))
+
+
+def average(param1, param2):
+    return (param1 + param2)/2
+
+
+
+
+
+print(average(2,3))
+
+
+
---
+++ NCIRL/Big Data Programming/Factorial.py
@@ -0,0 +1,33 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      finnp
+#
+# Created:     07/10/2016
+# Copyright:   (c) finnp 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def sumfact(number):
+    sumfact = 0
+    for i in range (1, number +1):
+        sumfact=sumfact + i
+    return sumfact
+
+def factorial(number):
+    result = 1
+    for i in range (1, number+1):
+        result = result * i
+    return result
+
+
+def combin(set, selection):
+    #Combinatorial Probability from Thursday 6th OCt
+    # P(Event) = N!/n!(N-n)!
+    return (factorial(set)/(factorial(selection)* factorial(set - selection)))
+
+
+print (sumfact(6))
+print(factorial(6))
+print(combin(6,2))
